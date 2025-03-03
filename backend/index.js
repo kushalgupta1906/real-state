47e320cad847e5ec29e48378f33a99fc87764642
kushalgupta1906 <kushalgupta1906@gmail.com>
2025-03-04 01:55:40 +0530

    change index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.listen(5001, ()=>{
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
 app.use('/api/listing',listingRouter);
-app.use(express.static(path.join(_dirname,'/client/build')));
+app.use(express.static(path.join(_dirname,'/client/dist')));
 app.get('*',(req,res)=>{
     res.sendFile(path.join(_dirname,'client','dist','index.html'));
 })
